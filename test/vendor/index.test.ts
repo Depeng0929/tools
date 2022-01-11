67725a8894d0731c0c23444dff8ebfad19eba981
depeng.kong <15365182986@163.com>
2022-01-11 14:39:51 +0800

    test: change test

--- test/vendor/index.test.ts
+++ test/vendor/index.test.ts
@@ -7,11 +7,11 @@ describe('vendor', () => {
   })
 
   it('operation ok', () => {
-    expect(add(0.1, 0.3)).toBe(0.4)
-    expect(subtract(0.3, 0.1)).toBe(0.2)
-    expect(divide(0.3, 2)).toBe(0.15)
-    expect(multiply(0.1, 0.3)).toBe(0.03)
+    expect(add(0.1, 0.3)).toBe('0.4')
+    expect(subtract(0.3, 0.1)).toBe('0.2')
+    expect(divide(0.3, 2)).toBe('0.15')
+    expect(multiply(0.1, 0.3)).toBe('0.03')
 
-    expect(add('0.1', 0.3)).toBe(0.4)
+    expect(add('0.1', 0.3)).toBe('0.4')
   })
 })
