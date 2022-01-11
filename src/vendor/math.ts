942000e6aeb8467bcb9feef3e1d0719d72eb5555
depeng.kong <15365182986@163.com>
2022-01-11 14:32:21 +0800

    fix: change math to big.js

--- src/vendor/math.ts
+++ src/vendor/math.ts
@@ -1,24 +1,19 @@
-import * as math from 'mathjs'
-import type { MathType } from 'mathjs'
+import Big from 'big.js'
 
 type numberLike = string | number
 
-function format(a: MathType) {
-  return math.number(math.format(a))
-}
-
 export function add(a: numberLike, b: numberLike) {
-  return format(math.add(math.bignumber(a), math.bignumber(b)))
+  return Big(a).plus(b).toString()
 }
 
 export function subtract(a: numberLike, b: numberLike) {
-  return format(math.subtract(math.bignumber(a), math.bignumber(b)))
+  return Big(a).minus(b).toString()
 }
 
 export function multiply(a: numberLike, b: numberLike) {
-  return format(math.multiply(math.bignumber(a), math.bignumber(b)))
+  return Big(a).times(b).toString()
 }
 
 export function divide(a: numberLike, b: numberLike) {
-  return format(math.divide(math.bignumber(a), math.bignumber(b)))
+  return Big(a).div(b).toString()
 }
