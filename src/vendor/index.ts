0ecb9fd7e194e8ad6121d0b88a8160241936bec9
depeng.kong <15365182986@163.com>
2022-01-13 14:40:44 +0800

    feat: delete ramda

--- src/vendor/index.ts
+++ src/vendor/index.ts
@@ -1,2 +1,1 @@
-export * from './ramda'
 export * from './math'
--- test/vendor/index.test.ts
+++ test/vendor/index.test.ts
@@ -1,11 +1,6 @@
-import { add, divide, findValueByKey, multiply, subtract } from '../../src'
+import { add, divide, multiply, subtract } from '../../src'
 
 describe('vendor', () => {
-  it('findValueByKey', () => {
-    expect(findValueByKey()(2, [{ key: 1, value: '1' }, { key: 2, value: '2' }])).toBe('2')
-    expect(findValueByKey()(3, [{ key: 1, value: '1' }, { key: 2, value: '2' }])).toBeUndefined()
-  })
-
   it('operation ok', () => {
     expect(add(0.1, 0.3)).toBe('0.4')
     expect(subtract(0.3, 0.1)).toBe('0.2')
