f476885febc1def5034925e5e16efd380ecf6aee
depeng.kong <15365182986@163.com>
2022-06-06 21:46:27 +0800

    feat: string name transform

--- src/string.ts
+++ src/string.ts
@@ -33,3 +33,24 @@ export function randomStr(size = 16, dict = urlAlphabet) {
     id += dict[(Math.random() * len) | 0]
   return id
 }
+
+// 大驼峰
+export function pascalCase(str: string) {
+  return capitalize(camelCase(str))
+}
+
+// 小驼峰
+export function camelCase(str: string) {
+  return str.replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ''))
+}
+
+// 短横线
+export function kebabCase(key: string) {
+  const result = key.replace(/([A-Z])/g, ' $1').trim()
+  return result.split(' ').join('-').toLowerCase()
+}
+
+// 首字母大写
+export function capitalize(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
