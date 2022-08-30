b956fd7d8bbe278522f74860213e842f9130664d
depeng.kong <15365182986@163.com>
2022-08-30 18:00:58 +0800

    style: lint

--- src/vendor/day.ts
+++ src/vendor/day.ts
@@ -6,27 +6,27 @@
  * @param format - YYYY-MM-DD HH:mm:ss
  */
 export function formateTime(time: string | number | Date, format = 'YYYY-MM-DD HH:mm:ss'): string {
-  var date: Date = time ? new Date(time) : new Date();
-  var o = {
-    "M+": date.getMonth() + 1, //月份
-    "D+": date.getDate(), //日
-    "H+": date.getHours(), //小时
-    "m+": date.getMinutes(), //分
-    "s+": date.getSeconds() //秒
+  const date: Date = time ? new Date(time) : new Date()
+  const o = {
+    'M+': date.getMonth() + 1, // 月份
+    'D+': date.getDate(), // 日
+    'H+': date.getHours(), // 小时
+    'm+': date.getMinutes(), // 分
+    's+': date.getSeconds(), // 秒
   } as any
   if (/(Y+)/.test(format)) {
     format = format.replace(
       RegExp.$1,
-      (date.getFullYear() + "").substr(4 - RegExp.$1.length)
-    );
+      (`${date.getFullYear()}`).substr(4 - RegExp.$1.length),
+    )
   }
-  for (var k in o) {
-    if (new RegExp("(" + k + ")").test(format)) {
+  for (const k in o) {
+    if (new RegExp(`(${k})`).test(format)) {
       format = format.replace(
         RegExp.$1,
-        RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length)
-      );
+        RegExp.$1.length == 1 ? o[k] : (`00${o[k]}`).substr((`${o[k]}`).length),
+      )
     }
   }
-  return format;
+  return format
 }
