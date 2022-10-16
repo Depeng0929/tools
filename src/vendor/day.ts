6cf62b3c67daeb4e896a5580bb837c69e0ca608f
depeng.kong <15365182986@163.com>
2022-10-16 19:37:17 +0800

    fix: formateTime error

--- src/vendor/day.ts
+++ src/vendor/day.ts
@@ -1,32 +1,5 @@
-/**
- * formate time by dayjs
- *
- * @see https://dayjs.fenxianglu.cn/category/display.html#%E6%A0%BC%E5%BC%8F%E5%8C%96
- * @param time - string | number | Date | dayjs.Dayjs | null | undefined
- * @param format - YYYY-MM-DD HH:mm:ss
- */
-export function formateTime(time: string | number | Date, format = 'YYYY-MM-DD HH:mm:ss'): string {
-  const date: Date = time ? new Date(time) : new Date()
-  const o = {
-    'M+': date.getMonth() + 1, // 月份
-    'D+': date.getDate(), // 日
-    'H+': date.getHours(), // 小时
-    'm+': date.getMinutes(), // 分
-    's+': date.getSeconds(), // 秒
-  } as any
-  if (/(Y+)/.test(format)) {
-    format = format.replace(
-      RegExp.$1,
-      (`${date.getFullYear()}`).substr(4 - RegExp.$1.length),
-    )
-  }
-  for (const k in o) {
-    if (new RegExp(`(${k})`).test(format)) {
-      format = format.replace(
-        RegExp.$1,
-        RegExp.$1.length == 1 ? o[k] : (`00${o[k]}`).substr((`${o[k]}`).length),
-      )
-    }
-  }
-  return format
+import dayjs from 'dayjs'
+
+export function formateTime(time: number | Date, format = 'YYYY-MM-DD HH:mm'): string {
+  return dayjs(time).format(format)
 }
