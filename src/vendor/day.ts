185dc2e2c967cc7058424e7c0f98cd3b01bc30af
depeng.kong <15365182986@163.com>
2023-02-27 09:49:39 +0800

    chore: update deps

--- src/vendor/day.ts
+++ src/vendor/day.ts
@@ -1,4 +1,4 @@
-import dayjs from 'dayjs'
+import dayjs from 'dayjs/esm/index.js'
 
 export function formateTime(time: number | Date, format = 'YYYY-MM-DD HH:mm'): string {
   return dayjs(time).format(format)
